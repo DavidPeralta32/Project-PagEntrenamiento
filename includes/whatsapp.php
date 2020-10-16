63ec8ce8625eab746b6986ce253c9280ea35bbb8
DavidPeralta <davi32peralta@gmail.com>
2020-10-16 14:45:18 -0500

    Cambieo prueba

--- includes/whatsapp.php
+++ includes/whatsapp.php
@@ -1,4 +1,4 @@
 <!-- uso de la api de whatsapp -->
-<a class="appWhatsapp" target="_blank" href="https://api.whatsapp.com/send?phone=2881318138&text=Hola!&nbsp;me&nbsp;pueden&nbsp;apoyar?">
+<a class="appWhatsapp" target="_blank" href="https://api.whatsapp.com/send?phone=+522291346424&text=Hola!&nbsp;me&nbsp;pueden&nbsp;apoyar?">
 <i class="fab fa-whatsapp"></i>
 </a>
