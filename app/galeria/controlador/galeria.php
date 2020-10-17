97a9974b066b0142d1b1c05ad26a482ee9f14ccc
DavidPeralta <davi32peralta@gmail.com>
2020-10-16 23:51:19 -0500

    Empiezo la galaeria

---
+++ app/galeria/controlador/galeria.php
@@ -0,0 +1,7 @@
+<?php
+
+
+
+
+require_once '../views/galeria.view.php';
+?>
---
+++ app/galeria/views/galeria.view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/32258d33f0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../../css/style.css">
+    <title>Galeria</title>
+</head>
+<body>
+    <div class="galeria">
+    <h1>Galeria de fotos</h1>
+        <div class="fotos">
+         <h1>Foto 1</h1>
+            <img src="../fotos/equipoderro.jpg" alt="">
+        </div>
+        <div class="fotos">
+         <h1>Foto 1</h1>
+            <img src="../fotos/equipoderro.jpg" alt="">
+        </div>
+        <div class="fotos">
+         <h1>Foto 1</h1>
+            <img src="../fotos/equipoderro.jpg" alt="">
+        </div>
+        <div class="fotos">
+         <h1>Foto 1</h1>
+            <img src="../fotos/equipoderro.jpg" alt="">
+        </div>
+        <div class="fotos">
+         <h1>Foto 1</h1>
+            <img src="../fotos/equipoderro.jpg" alt="">
+        </div>
+        <div class="fotos">
+         <h1>Foto 1</h1>
+            <img src="../fotos/equipoderro.jpg" alt="">
+        </div>
+    </div>
+    <div class="footer">
+        <div class="regresar">
+            <a href=""><i class="far fa-arrow-alt-circle-left"></i></a>
+        </div>
+        <div class="siguiente">
+            <a href=""><i class="far fa-arrow-alt-circle-right"></i></a>
+        </div>
+    </div>
+</body>
+</html>
