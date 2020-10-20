85561f5725cb4385875d08b59896d443ed0420c0
DavidPeralta <davi32peralta@gmail.com>
2020-10-19 23:38:32 -0500

    creo vista subir foto

---
+++ app/galeria/views/subir.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../css/style.css">
+    <title>Subir foto</title>
+</head>
+<body>
+    <h1>Subir foto</h1>
+    <div class="contenedor-galeria">
+        <div class="contenedor-subir">
+            <input type="file" name="Seleccionar archivo" id="">
+            <label for="">Numero de control:</label>
+            <input type="text">
+            <label for="">Grupo:</label>
+            <input type="text">
+            <input type="submit" value="Subir">
+        </div>
+    </div>
+    <div class="footer-subir">
+        <ul>
+            <li> <a href="../../entrenar/index.php" class="link">Regresar</a></li>
+            <li> <a href="../controlador/galeria.php" class="link">Galeria</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
---
+++ includes/apiYoutube/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Youtube</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+
+<?php
+    $API_key    = 'AIzaSyB-j7aCZ0DLyGNBPnjVLw6fzwylT_LYG1g';
+    $channelID  = 'UCeevP_v2zbJl4oSpK3iiEWw';
+    $maxResults = 5;
+
+    $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelID.'&maxResults='.$maxResults.'&key='.$API_key.''));
+
+?>
+    <div class="contenedor-api">
+        <?php 
+            foreach($videoList->items as $item){
+                //Embed video
+                if(isset($item->id->videoId)){
+                    echo '<div class="youtube-video">
+                            <iframe width="280" height="200" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
+                            <h2>'. $item->snippet->title .'</h2>
+                            <h2>'.$item->snippet->description .'</h2>
+                        </div>';
+                }
+            }
+        ?>
+    </div>
+</body>
+</html>
