536850178de3ac3b1d61d5a87bab0987d68323c1
David Peralta <davi32peralta@gmail.com>
2021-03-21 23:33:16 -0600

    Probar git hosting

--- Administrador/views/index.view.php
+++ Administrador/views/index.view.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="./JS/indexView.js"></script>
-    <title>Lista de alumnos</title>
+    <title>Lista de alumnos Asu</title>
     <script>
             $(document).ready(function () {
                 listarUsuarios()
